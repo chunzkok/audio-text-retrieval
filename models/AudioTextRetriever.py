a9e4e54c86a20b7122c9e362c61354a3358693b1
chunzkok <kokcz@comp.nus.edu.sg>
2024-06-10 01:42:07 +0800

    Update AudioTextRetriever outputs to further follow HuggingFace Trainer specifications

--- models/AudioTextRetriever.py
+++ models/AudioTextRetriever.py
@@ -6,12 +6,16 @@ from TextEncoders import TextEncoder, RoBERTaEncoder
 from dataclasses import dataclass
 from torch import nn
 from transformers.utils import ModelOutput
-from typing import List, Optional, Union
+from typing import Any, Callable, List, Optional, Union
 
 @dataclass
 class AudioTextOutput(ModelOutput):
-    audio_embedding: torch.FloatTensor
-    text_embedding: torch.FloatTensor
+    # should be of shape (B, 2, E) where B = batch size, E = embedding dim. 
+    # slice [b, 0, :] should give audio embedding of item b in the batch
+    # slice [b, 1, :] should give textembedding of item b in the batch
+    embeddings: torch.FloatTensor 
+
+    loss: torch.FloatTensor = None
 
     def __getitem__(self, key):
         if key == None: 
@@ -24,7 +28,7 @@ class AudioTextOutput(ModelOutput):
         return getattr(self, key)
        
     def to_tuple(self):
-        return (self.audio_embedding, self.text_embedding)
+        return (self.embeddings, self.loss)
 
 
 class AudioTextRetriever(nn.Module):
@@ -33,6 +37,7 @@ class AudioTextRetriever(nn.Module):
     DEFAULT_TEXT_ENCODER = lambda: RoBERTaEncoder(2048, 1024)
 
     def __init__(self, 
+                 loss_fn: Callable[[torch.FloatTensor, torch.FloatTensor], torch.FloatTensor],
                  text_enc: Optional[TextEncoder] = None, 
                  audio_enc: Optional[AudioEncoder] = None,
                  num_heads: Optional[int] = 8,
@@ -46,6 +51,7 @@ class AudioTextRetriever(nn.Module):
             text_enc if text_enc is not None 
             else AudioTextRetriever.DEFAULT_TEXT_ENCODER()
         )
+        self.loss_fn = loss_fn
 
         # both are mapping to the same embedding space, so both encoders must have same output dimensions
         assert self.TextEncoder.out_dim == self.AudioEncoder.out_dim
@@ -70,7 +76,9 @@ class AudioTextRetriever(nn.Module):
     def forward(self,
                 raw_speech: Union[np.ndarray, List[float], List[np.ndarray], List[List[float]]], 
                 sentence: Union[str, List[str], List[List[str]]], 
-                sampling_rate: Optional[int] = 16000, 
+                sampling_rate: Optional[int] = 16000,
+                return_dict: Optional[bool] = True,
+                labels: Any = None
                 ) -> torch.FloatTensor:
         audio_embed = self.AudioEncoder.preprocess(raw_speech, sampling_rate)
         print(f"audio preprocess dims: {audio_embed.input_values.shape}")
@@ -91,4 +99,8 @@ class AudioTextRetriever(nn.Module):
         text_embed = layer_norm(self.TextAttention(text_embed, audio_embed_raw, audio_embed_raw)[0] + audio_embed_raw)
         text_embed = layer_norm(self.TextAttentionFF(text_embed) + text_embed)
 
-        return AudioTextOutput(audio_embed, text_embed)
+        embeddings = torch.stack((audio_embed, text_embed))
+        loss = self.loss_fn(audio_embed, text_embed)
+
+        return (AudioTextOutput(embeddings=embeddings, loss=loss) if return_dict 
+                else (loss, embeddings)) # Trainer specifies to return loss as first element if a tuple is returned
--- models/test.py
+++ models/test.py
@@ -9,6 +9,7 @@ def speech_file_to_array(path: str, target_rate: int) -> np.ndarray:
   return speech.squeeze().numpy()
 
 eval_audio_path = "../../datasets/clotho/evaluation/Santa Motor.wav"
+eval_audio_path2 = "../../datasets/clotho/evaluation/105bpm.wav"
 eval_captions = ["A machine whines and squeals while rhythmically punching or stamping.",
         "A person is using electric clippers to trim bushes.",
         "Someone is trimming the bushes with electric clippers.",
@@ -17,7 +18,10 @@ eval_captions = ["A machine whines and squeals while rhythmically punching or st
 ]
 SAMPLE_RATE = 16_000
 
-retriever = AudioTextRetriever() # use default
+retriever = AudioTextRetriever(loss_fn=lambda x, y: x.sum() + y.sum()) # use default
 audio = speech_file_to_array(eval_audio_path, SAMPLE_RATE)
+audio2 = speech_file_to_array(eval_audio_path2, SAMPLE_RATE)
 
-print(retriever(audio, SAMPLE_RATE, eval_captions[0]))
+output = retriever([audio, audio2], eval_captions[0:2], SAMPLE_RATE)
+print(output)
+print(output["embeddings"].shape)
