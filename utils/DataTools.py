54246c002bc02507faebf018cac0d5d9dd1b8c05
chunzkok <kokcz@comp.nus.edu.sg>
2024-06-13 23:10:14 +0800

    Fix contrastive loss function and type checking for all files

--- utils/DataTools.py
+++ utils/DataTools.py
@@ -1,8 +1,12 @@
 import numpy as np
+import torch
 from typing import Callable, List, Optional, Union
 
 class AudioTextDataCollator:
-    def __init__(self, path_to_audio_converter: Callable[[Union[str, List[str]], Union[np.ndarray, List[np.ndarray]]]], k: int):
+    def __init__(
+            self, 
+            path_to_audio_converter: Callable[[Union[str, List[str]]], Union[np.ndarray, List[np.ndarray], torch.Tensor]], 
+            k: int):
         self.path_to_audio_converter = path_to_audio_converter # should output at the correct sampling rate too!
         self.k = k # Number of negative samples per audio file
 
@@ -30,6 +34,7 @@ class AudioTextDataCollator:
                 batch["labels"].append(1)
 
             neg_indices = self._random_index_excluding(N, i, self.k)
+            assert type(neg_indices) == np.ndarray # hacky solution, we are only using clotho which should have 5 captions
             for j in neg_indices:
                 batch["raw_audio"].append(audio)
                 if type(caption[j] == list):
@@ -40,7 +45,12 @@ class AudioTextDataCollator:
 
         return batch
 
-    def _random_index_excluding(upper_bound: int, exclude: int, size: Optional[int] = None):
+    def _random_index_excluding(
+            self, 
+            upper_bound: int, 
+            exclude: int, 
+            size: Optional[int] = None
+        ) -> Union[int, np.ndarray]:
         if size is not None:
             indices = np.random.randint(upper_bound - 1, size=size)
             return indices + (indices >= exclude)
--- utils/LossFunctions.py
+++ utils/LossFunctions.py
@@ -1,26 +1,23 @@
-import numpy as np
 import torch
 
 from torch.nn import functional
+from typing import Optional
 
-CONTRASTIVE_TEMP = 0.2 # from the pdf
 
-def contrastiveCE(audio_embed: torch.FloatTensor, text_embed: torch.FloatTensor, labels: torch.Tensor):
-    num = (audio_embed @ text_embed.T) / CONTRASTIVE_TEMP
+def contrastiveCE(
+        audio_embed: torch.FloatTensor, 
+        text_embed: torch.FloatTensor, 
+        labels: torch.Tensor, 
+        temperature: Optional[float] = 0.2
+    ):
 
-    denom_audio = (audio_embed @ text_embed.T) / CONTRASTIVE_TEMP
-    denom_audio = np.exp(denom_audio)
-    denom_audio = np.sum(denom_audio, axis=1)
-    denom_audio = np.log(denom_audio)
+    # wait... aren't they just transposes of each other
+    logits_audio2text = audio_embed @ text_embed.T / temperature
+    logits_text2audio = text_embed @ audio_embed.T / temperature
 
-    denom_text = (text_embed @ audio_embed.T) / CONTRASTIVE_TEMP
-    denom_text = np.exp(denom_text)
-    denom_text = np.sum(denom_text, axis=1)
-    denom_text = np.log(denom_text)
-
-    loss = (functional.cross_entropy(num - denom_audio, labels)
-        + functional.cross_entropy(num - denom_text, labels)) / 2
-
-    return loss
+    return 1/2 * (
+        functional.cross_entropy(logits_audio2text, labels)
+        + functional.cross_entropy(logits_text2audio, labels)
+    )
 
 
